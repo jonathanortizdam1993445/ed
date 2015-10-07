05c1b3b9c1083d585c3d6e01ed2bda746200ed47
jonathanortizdam1993445 <jonathanortizisa@hotmail.com>
2015-10-07 09:07:05 +0200

    hola-mundo.git.txt

--- JHolaMundo/src/org/institutoserpis/ed/HolaMundo.java
+++ JHolaMundo/src/org/institutoserpis/ed/HolaMundo.java
@@ -1,9 +1,11 @@
 package org.institutoserpis.ed;
-
+import java.util.Scanner;
 public class HolaMundo {
-
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo desde eclipse");
+		Scanner tcl =new Scanner (System.in);
+		System.out.println("como te llamas");
+		int nombre=tcl.nextInt();
+		System.out.println("Hola Mundo desde eclipse");	
 		
 	}
 
